7a537854dbd2c6108a3633e85473eea04c07d1a3
倉田翔 <kd1321373@st.kobedenshi.ac.jp>
2024-05-14 13:24:11 +0900

    post

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="input.html">text形式の送信練習</a>
+<a href="radio.html">ラジオボタンの送信練習</a>
+<a href="checkbox.html">チェックボックスの送信練習</a>
+
+</body>
+</html>
